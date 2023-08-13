93da6e3ae9fbf72cab25c12175547c77f5fc6ac4
PulkitBxtra <PulkitBxtra@users.noreply.github.com>
2023-08-14 01:12:19 +0530

    fibb solution

--- DP/dp.cpp
+++ DP/dp.cpp
@@ -1,26 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int fibb(int n, vector<int> &dp){
 
-    if(n==1 || n==2){
-        return n+1;
-    }
-
-    if(dp[n]!=-1){
-        return dp[n];
-    }
-
-    return dp[n]= fibb(n-1,dp)+ fibb(n-2,dp);
-}
-
+//******************************** Tabular Solution **************************
 
 int main(){
-
     int n=5;
 
-    vector<int> dp(n+1,-1);
+    int prev1=1;
+    int prev2=0;
 
-    cout<<fibb(5,dp)<<endl;
+    for(int i=0;i<=n;i++){
+        int prev=prev1+prev2;
+        prev2=prev1;
+        prev1=prev;
+    }
 
+    cout<<prev1<<endl;
 }
+
+
+
+// **************************** Memoisation Solution **************************
+
+
+// int fibb(int n, vector<int> &dp){
+
+//     if(n==1 || n==2){
+//         return n+1;
+//     }
+
+//     if(dp[n]!=-1){
+//         return dp[n];
+//     }
+
+//     return dp[n]= fibb(n-1,dp)+ fibb(n-2,dp);
+// }
+
+
+// int main(){
+
+//     int n=5;
+
+//     vector<int> dp(n+1,-1);
+
+//     cout<<fibb(5,dp)<<endl;
+
+// }
